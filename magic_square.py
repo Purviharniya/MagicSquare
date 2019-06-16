e3b3a94dd4e0bdb046db2315aad484e78d395003
Purviharniya <42908278+Purviharniya@users.noreply.github.com>
2019-06-16 10:43:33 +0530

    Update and rename README.md to magic_square.py

---
+++ magic_square.py
@@ -0,0 +1,27 @@
+# MagicSquare
+n=int(input("Enter the order of matrix : "))
+sum=int(n*(n*n+1)/2)
+mat=[]
+row=[]
+mat=[[0 for i in range(n)]for j in range(n)]
+for k in range(1,n*n+1):
+    if k==1:
+        i,j=n//2,n-1
+    else :
+        i-=1
+        j+=1
+        if i==-1 and j !=n:
+            i=n-1
+        elif i !=-1 and j==n:
+            j=0
+        elif i==-1 and j==n:
+            i=0
+            j=n-2
+        if mat[i][j] !=0:
+            i+=1
+            j-=2
+    mat[i][j] = k
+for i in range(n):
+    for j in range(n):
+        print(mat[i][j],end=" ")
+    print()
